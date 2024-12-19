abff4ca78cd2d934382421c3934baddf5243e69d
Chintan Panchal <chintanpanchal396@gmail.com>
2024-12-19 14:55:23 -0500

    Added Tick Tick Done

---
+++ Tick Tick Done/Tick Tick Done/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tick Tick Done
+//
+//  Created by Chintan Panchal on 19/12/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
